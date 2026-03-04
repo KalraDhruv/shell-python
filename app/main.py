a0827e537266472489ece98626ce6094c4fe3e9b
Dhruv Kalra <dhruvkalra@proton.me>
2026-03-04 10:58:52 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 import sys
 
+def commands(terminal_input):
+    if terminal_input[0:5] == "echo ":
+        print(terminal_input[5:])
+    else:
+        print(f"{terminal_input}: command not found")
+
+
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
@@ -8,7 +15,8 @@ def main():
         terminal_input = input()
         if terminal_input == "exit":
             break
-        print(f"{terminal_input}: command not found")
+        else: 
+            commands(terminal_input)
     pass
 
 

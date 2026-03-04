6d6e7ad931e983fd62738eb72e8d0daebd19421f
Dhruv Kalra <dhruvkalra@proton.me>
2026-03-04 10:45:04 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    terminal_input = input()
+    print(f"{terminal_input}: command not found")
     pass
 
 
